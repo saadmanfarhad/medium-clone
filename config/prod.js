ceee2c669e532e9c1c75ea9fef690f1fdb984964
Samiha Nanjiba <snanjiba@gmail.com>
2020-08-16 19:55:20 +0600

    Added Google Authentication

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,11 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cloudinaryCloudName: '',
   cloudinaryApiKey: '',
-  cloudinaryApiSecret: ''
+  cloudinaryApiSecret: '',
+
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  redirectURI: process.env.REDIRECT_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
--- models/User.js
+++ models/User.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
 let UserSchema = new mongoose.Schema({
+  googleID: String,
   name: String,
   email: String,
   provider: String,
-  provider_id: String,
-  token: String,
-  provider_pic: String,
+  profile_pic: String,
   followers: [
     {
       type: mongoose.Schema.Types.ObjectId,
